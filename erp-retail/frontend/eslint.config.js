5c22cee8f20db97150d4e3b13d74dadfa4ee0a49
vutiendat <vutiendat23022005@gmail.com>
2025-08-12 16:25:04 +0700

    update-frontend-h1

--- erp-retail/frontend/eslint.config.js
+++ erp-retail/frontend/eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import tseslint from 'typescript-eslint'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -10,6 +11,7 @@ export default defineConfig([
     files: ['**/*.{js,jsx}'],
     extends: [
       js.configs.recommended,
+      tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
---
+++ erp-retail/frontend/src/pages/Warehouse.tsx
@@ -0,0 +1,110 @@
+import React, {useState, useEffect} from 'react';
+
+import { Button } from '../components/ui/button';
+
+import ProductStatic from '../components/inventory_components/products/ProductStatic';
+import { ProductSearch } from '../components/inventory_components/products/ProductSearch';
+import { WarehouseChoose } from '../components/inventory_components/warehouses/WarehouseChoose';
+import { getWarehouses } from '../services/inventery-api/WarehouseService';
+import { WarehouseTableComponent } from '../components/inventory_components/warehouses/WarehouseTable';
+import { getInventoryByNameWarehouse } from '../services/inventery-api/WarehouseService';
+interface Warehouse {
+  id: string;
+  name: string;
+  description: string;
+  address: string;
+  status: string;
+  createBy: string;
+  updateBy: string;
+}
+
+interface Inventory {
+  id: string;
+  productName: string;
+  quantityAvailable: number;
+  warehouseName: string;
+  status: string;
+  minimumQuantity: number;
+  maximumQuantity: number;
+  productBatchName: string;
+}
+
+const Warehouse: React.FC = () => {
+  const [dataWarehouse, setDataWarehouse] = useState<Warehouse[]>([]);
+  const [openFindWarehouse, setOpenWarehouse] = useState(false);
+  const [selectWarehouse, setSelectWarehouse] = useState<string | null>('');
+  const [inventories, setInventories] = useState<Inventory[]>([]);
+
+  useEffect(() => {
+    const fetchWarehouses = async () => {
+      try {
+        const response = await getWarehouses();
+        const warehouseData: Warehouse[] = response.data;
+        setDataWarehouse(warehouseData);
+
+        if (warehouseData.length > 0) {
+          setSelectWarehouse(warehouseData[0].id);
+        }
+      } catch (error) {
+      console.error("Failed to fetch warehouse data: ", error);
+      }
+    };
+
+    fetchWarehouses();
+  }, []);
+
+  useEffect(() => {
+    const fetchInventories = async () => {
+      if (selectWarehouse) {
+        try {
+          const response = await getInventoryByNameWarehouse(selectWarehouse);
+          setInventories(response.data);
+        } catch (error) {
+          console.error("Failed to fetch inventory data: ", error);
+        }
+      }
+    };
+
+    fetchInventories();
+  }, [selectWarehouse]);
+
+  return (
+    <div>
+      <div className='px-6 md:px-10 -mt-10 space-y-6'>
+        <div className='flex items-center justify-between '>
+          <div className='mb-2'>
+            <h3 className='mb-6'>Quản lý kho hàng</h3>
+            <p className="text-muted-foreground">
+              Theo dõi và quản lý tồn kho của bạn
+            </p>
+          </div>
+        
+          <div>
+            <WarehouseChoose
+              selectWarehouse={selectWarehouse}
+              setSelectWarehouse={setSelectWarehouse}
+              openFindWarehouse={openFindWarehouse}
+              setOpenWarehouse={setOpenWarehouse}
+              dataWarehouse={dataWarehouse}
+            />
+          </div>
+        </div>
+
+        <div>
+          <ProductStatic/>
+        </div>
+
+        <div>
+          <ProductSearch />
+        </div>
+
+        <div className='mt-6'>
+          <WarehouseTableComponent
+            data={inventories}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Warehouse;
