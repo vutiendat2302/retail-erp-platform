b4782cc57cacbcfcbf2ab5358bd564336c771adc
vutiendat2302 <vutiendat23022005@gmail.com>
2025-09-05 22:34:18 +0700

    update-frontend

--- erp-retail/frontend/src/services/pos-api/api.tsx
+++ erp-retail/frontend/src/services/pos-api/api.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8083',
+  baseURL: 'http://localhost:8084',
   headers: {
     'Content-Type': 'application/json'
   }
